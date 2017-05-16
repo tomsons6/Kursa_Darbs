2b6b12473e55ab6b53775532206937eeaf10bf4d
tomsons6 <toms.amsons@va.lv>
2017-05-17 00:55:50 +0300

    Darba izveide ar bagiem

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public bool colis = true;
+
+    [SerializeField]
+    private GameObject cube;
+
+    private void Start()
+    {
+        StartCoroutine(CubeBehaviour());
+
+        instance = this;
+    }
+    private void Update()
+    {
+
+    }
+
+    private IEnumerator CubeBehaviour()
+    {
+
+        Debug.Log("Changing cube color");
+        if (colis == false)
+        {
+
+        }
+        if (colis == true)
+        {
+            while (colis == true)
+            {
+                yield return ChangeToColor(Color.green, Color.blue); //Waiting until Change color done
+                Debug.Log("CubeBehaviour done!");
+                yield return ChangeToColor(Color.blue, Color.green); //Waiting until Change color done
+                Debug.Log("CubeBehaviour done!");
+            }
+        }
+    }
+
+
+    public IEnumerator ChangeToColor(Color targetColor, Color targetColor1)
+    {
+        float t = 0f; //time variable that will hold the time between 0..1
+
+        Renderer renderer = cube.GetComponent<Renderer>();
+
+        Color originalColor = renderer.material.color;
+
+        while (t < 1f) //while time is not over or equal to 1f (100%)
+        {
+            t += Time.deltaTime; // increase percentage by 1/fps
+
+            Color newColor = Color.Lerp(originalColor, targetColor, t); //Interpolate linearly between original color and target color using T as percentage
+            renderer.material.color = newColor; // set new color
+            yield return null;// wait for the next frame
+        }
+        if (targetColor == Color.green)
+        {
+            Color originalColor1 = renderer.material.color;
+            while (t < 1f) //while time is not over or equal to 1f (100%)
+            {
+                t += Time.deltaTime; // increase percentage by 1/fps
+
+                Color newColor = Color.Lerp(originalColor1, targetColor1, t); //Interpolate linearly between original color and target color using T as percentage
+                renderer.material.color = newColor; // set new color
+                yield return null;// wait for the next frame
+            }
+        }
+        if (targetColor1 == Color.blue)
+
+        {
+            Color originalColor2 = renderer.material.color;
+            while (t < 1f) //while time is not over or equal to 1f (100%)
+            {
+                t += Time.deltaTime; // increase percentage by 1/fps
+
+                Color newColor = Color.Lerp(originalColor2, targetColor, t); //Interpolate linearly between original color and target color using T as percentage
+                renderer.material.color = newColor; // set new color
+                yield return null;// wait for the next frame
+            }
+
+        }
+    }
+}
+
+    /// <summary>
+    /// When current state is changed - state machine will react
+    /// </summary>
+    /// <returns></returns>
+
---
+++ Assets/Script/PowerUp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PowerUp : MonoBehaviour
+{
+    
+    public GameObject FPSpref;
+    public GameObject startpoint;
+    public Transform prefab;
+    public Vector3 pos;
+    public int count;
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+
+        
+        if (hit.gameObject.tag == "Cube")
+        {
+            count++;
+            GameManager.instance.colis = false;
+            Vector3  pos= gameObject.transform.position;
+            Debug.Log(count.ToString());
+            if (count == 2 )
+            {
+                gameObject.GetComponentInChildren<Camera>().rect = new Rect(.5f, 0f, .5f, 1f);
+                Instantiate(prefab, pos + new Vector3(-3, 0, -3), Quaternion.identity);
+                prefab.GetComponentInChildren<Camera>().rect = new Rect(0f, 0f, .5f, 1f);
+
+
+            }
+            if (count == 3)
+            {
+                gameObject.GetComponentInChildren<Camera>().rect = new Rect(.5f, .5f, .5f, .5f);
+                gameObject.GetComponentInChildren<Camera>().rect = new Rect(.5f, 0f, .5f, .5f);
+                Instantiate(prefab,pos + new Vector3(3, 0, -3), Quaternion.identity);
+                prefab.GetComponentInChildren<Camera>().rect = new Rect(0f, 0f, .5f, .5f);
+                Instantiate(prefab, pos + new Vector3(-6, 0, -3), Quaternion.identity);
+                prefab.GetComponentInChildren<Camera>().rect = new Rect(0f, .5f, .5f, 5f);
+            }
+            Destroy(hit.gameObject);
+            
+        }
+        
+    }
+
+}
